b567e49fb6718ad70806db2b0f268507671bbfff
Zach  Smith <zdsmith001@gmail.com>
2019-07-14 18:48:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    array[-1] = "and " + array[-1]
+    return array.join(", ")
+  end
 end
