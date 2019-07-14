1098e4ebed4278038628171a4e210ae383c461be
Zach  Smith <zdsmith001@gmail.com>
2019-07-14 18:49:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return array[0]
   elsif array.length == 2
-    array.join(" and ")
+    return array.join(" and ")
   elsif array.length > 2
     array[-1] = "and " + array[-1]
     return array.join(", ")
